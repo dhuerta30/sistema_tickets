a09fcd828a51553cc7dd92e96a71db4ea7baba1b
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2025-09-23 11:30:58 -0300

    up

---
+++ app/core/cache/34cab588a6f51a0de33665ce1665f1b2.php
@@ -0,0 +1,22 @@
+<?php include 'C:\xampp\htdocs\sistema_tickets\app\core/cache/bc43069e62f27db794193766586b71fb.php'; ?>
+<?php include 'C:\xampp\htdocs\sistema_tickets\app\core/cache/f9f744bb1c61fdeaa90f01b98fdffdbc.php'; ?>
+<link href='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>css/sweetalert2.min.css' rel="stylesheet">
+<div class="content-wrapper">
+    <section class="content">
+        <div class="card mt-4">
+            <div class="card-body">
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php echo $render; ?>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+</div>
+<div id="artify-ajax-loader">
+    <img width="300" src='<?php echo htmlspecialchars($_ENV["BASE_URL"], ENT_QUOTES, 'UTF-8'); ?>app/libs/artify/images/ajax-loader.gif' class="artify-img-ajax-loader"/>
+</div>
+<?php include 'C:\xampp\htdocs\sistema_tickets\app\core/cache/0b27e2778a539e2c7b14342c3b0c5b16.php'; ?>
