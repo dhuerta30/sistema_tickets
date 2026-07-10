a85b3f1c4380a5c9ed3af971bbc9f0254935f5a0
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2026-07-10 15:00:24 -0400

    up

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3247,28 +3247,21 @@ class HomeController
 		$id_usuario_session = $_SESSION["usuario"][0]["id"];
 		
 		$queryfy = $obj->getQueryfyObj();
-
-		// Eliminar de usuario_menu
 		$queryfy->where("id_menu", $id_menu);
 		$queryfy->where("id_usuario", $id_usuario_session);
 		$queryfy->delete("usuario_menu");
 
-		// Buscar el id_submenu relacionado al id_menu
 		$queryfy->where("id_menu", $id_menu);
 		$id_menu_db = $queryfy->select("submenu");
 
-		// Verificar si se encontró el id_submenu
 		if (!empty($id_menu_db)) {
-			// Eliminar el submenu relacionado
 			$queryfy->where("id_submenu", $id_menu_db[0]["id_submenu"]);
 			$queryfy->delete("submenu");
 
-			// Eliminar de usuario_submenu relacionado
 			$queryfy->where("id_menu", $id_menu);
 			$queryfy->where("id_usuario", $id_usuario_session);
 			$queryfy->delete("usuario_submenu");
 		} else {
-			// Si no hay submenus, actualizar el campo "submenu" en la tabla menu
 			$queryfy->where("id_menu", $id_menu);
 			$queryfy->update("menu", array("submenu" => "No"));
 		}
